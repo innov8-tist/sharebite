7ef3335bf676078b83017362720c6aaa41b4382c
Naveenravi07 <gamerkidnav@gmail.com>
2024-04-03 13:31:29 +0530

    minor changes

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,18 +1,4 @@
-const { userModel } = require("../models/userModel")
-
 module.exports = {
-
-    signup: async (body) => {
-        let response = await new usermodel(body).save()
-        return response
-    },
-    login: async (body) => {
-        let userCheck = await userModel.findOne({ email: body.email, password: body.password })
-        if (!userCheck) {
-            return { status: false, }
-        }
-        return { ...userCheck, status: true }
-    },
     pagerender: (params) => {
         return new Promise((response, reject) => {
             console.log(params)
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var userhelper=require("../controllers/userController")
+const { login } = require('../controllers/userController.js');
 
 router.get('/', function(req, res, next) {
-  res.render('user/samples');
-  console.log("request reevie")
+    console.log("request reevie")
+    res.render('user/samples');
 });
 
+router.post('/login', async function(req, res, next) {
+    let user = await login(req.body)
+    if (!user.status) {
+        res.send("Login failed")
+    }else{
+        res.send("Login Success")
+    }
+});
 
 module.exports = router;
 
