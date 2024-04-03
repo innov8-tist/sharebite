dc1b360af36bf9df15b73470f4248fd05b19ba8a
Naveenravi07 <gamerkidnav@gmail.com>
2024-04-03 13:08:01 +0530

    auth functions

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,6 +1,20 @@
-module.exports={
-    pagerender:(params)=>{
-        return new Promise((response,reject)=>{
+const { userModel } = require("../models/userModel")
+
+module.exports = {
+
+    signup: async (body) => {
+        let response = await new usermodel(body).save()
+        return response
+    },
+    login: async (body) => {
+        let userCheck = await userModel.findOne({ email: body.email, password: body.password })
+        if (!userCheck) {
+            return { status: false, }
+        }
+        return { ...userCheck, status: true }
+    },
+    pagerender: (params) => {
+        return new Promise((response, reject) => {
             console.log(params)
             response()
         })
