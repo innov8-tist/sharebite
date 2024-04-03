5e0073ac0cf15727cf99649cfdabd7893410d506
Naveenravi07 <gamerkidnav@gmail.com>
2024-04-03 21:28:34 +0530

    admin table bug

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var adminhelper = require("../controllers/adminControllers")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    adminhelper.pagerender("hello").then(() => {
-        res.render("admin/oops", { admincommonfun: true });
-    })
+
 });
 
+router.get('/foodpickups',async function(req,res){
+    let data2 = await adminhelper.getAlllPickups();
+    res.render('admin/foodpickups',{data2:data2})
+})
 
 module.exports = router;
