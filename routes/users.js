559540e3f9b116dc3ad5ff476ac8752fe9a73662
Suraj <surajpa72@gmail.com>
2024-04-04 02:01:26 +0530

    home update again

--- routes/users.js
+++ routes/users.js
@@ -2,54 +2,31 @@ var express = require('express');
 var router = express.Router();
 const  userhelper  = require('../controllers/userController.js');
 
-
-var validitycheckingfun=function(req,res,next){
-  if(req.session.userlogin){
-   next()
-  }else{
-    res.redirect('/')
-  }
-
-}
-router.get('/home',(req,res)=>{
-  res.render("user/home");
-})
-
 router.get('/', function(req, res, next) {
-  if(req.session.loginfail){
-    console.log("true going");
-    res.render('user/login',{loginfail:true})
-    req.session.loginfail=false;
-  }else{
-    req.session.loginfail=false;
-    console.log("false going")
-    res.render('user/login',{loginfail:false});
-   
-  }
-
+    console.log("request reevie")
+    res.render('user/samples');
 });
 
-router.post('/userlogin',async function(req, res, next) {
+router.post('/userlogin', async function(req, res, next) {
   console.log(req.body)
    let user = await userhelper.login(req.body)
     if (!user.status) {
         console.log("loginfail")
-        req.session.loginfail=true;
-        res.redirect("/")
     }else{
-        req.session.userse=user._id;
-        res.render("user/home")
         console.log("loginned");
     }
+
+
 });
 
-router.post('/usersignup',async function(req, res, next) {
- await  userhelper.signup(req.body).then((data)=>{
-  console.log("heeeeyyyy")
-    console.log(data)
-  req.session.userse=data._id;
+router.get('/login', function(req, res, next) {
+  res.render('user/login')
+});
 
-   res.redirect('/home')
+router.post('/usersignup', async function(req, res, next) {
+ await  userhelper.signup(req.body).then((data)=>{
+    console.log(data);
+    console.log("successsfully completed");
    })
 
 });
@@ -59,14 +36,11 @@ router.get('/home', function(req, res, next) {
   console.log("Home Request");
 });
 
-router.get('/pickupdetails', async function(req, res) {
-    res.render('user/getPickupdetails');
-})
+router.get('/wallet', function(req, res, next) {
+  res.render('user/wallet', {user:true});
+  console.log("wallet Request");
+});
 
-router.post('/pickupdetails',async function(req,res){
-  let response = await userhelper.savePickupDetails(req.body)
-  res.json(response)
-})
 
 module.exports = router;
 
